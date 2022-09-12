00eb7d6417d3c79aa8df1e9d9b93a6caeea6c532
rencyngina <rencyngina@gmail.com>
2022-09-12 19:54:40 +0000

    i had to fix some few errors

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], x=0):
-    idx = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            idx += 1
-
-        except (ValueError, TypeError):
-            continue
-
-    print()
-    return idx
+def safe_print_division(a, b):
+    div = 0
+    try:
+        div = a/b
+        return div
+    except ZeroDivisionError:
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
