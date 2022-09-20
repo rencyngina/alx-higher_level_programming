df953098039594f1daf1f0a4ec78bc3688058129
rencyngina <rencyngina@gmail.com>
2022-09-20 21:51:49 +0000

    mostly answer texts

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+File: 101-locked_class.py
+Desc: This module contains one a class definition, LockedClass
+Author: Ilodiuba Victor (victornnamdii)
+Date Created: 26 Jul 2022
+"""
+
+
+class LockedClass:
+    """
+    This class prevents the user from dynamically creating new
+    instance attributes, except if the new instance attribute
+    is called first_name
+    """
+    __slots__ = ["first_name"]
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Traini
