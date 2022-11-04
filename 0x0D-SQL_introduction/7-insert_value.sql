a0d08c0f0315157a3b6125349eb9a35765a79501
rencyngina <rencyngina@gmail.com>
2022-11-04 04:16:59 -0700

    first add

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,5 +1,2 @@
 -- a script that inserts a new row in the table first_table
-INSERT INTO `first_table`(
-	id = 89,
-	name = BEST SCHOOL
-);
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
